8f3eb3fc7568e861075cb02c330e5fb0c611c6d0
sangwoo <tkddn3367@gmail.com>
2022-07-28 18:23:06 +0900

    set public path

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   outputDir: 'docs',
-  // publicPath: '/programming-materials'
+  publicPath: '/programming-materials'
 })
