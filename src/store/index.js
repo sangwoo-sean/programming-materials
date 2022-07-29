dded79d2915df301e4a7d00e2683a6b2f167307e
sangwoo <tkddn3367@gmail.com>
2022-07-29 15:29:13 +0900

    current Language Id bug fix -> String to Number

--- src/store/index.js
+++ src/store/index.js
@@ -16,7 +16,7 @@ export default new Vuex.Store({
   },
   mutations: {
     setCurrentLanguageId(state, id) {
-      state.currentLanguageId = id;
+      state.currentLanguageId = Number(id);
     }
   },
   actions: {
